16f6c02d249f2e227e0ee86d9bdd5b9ffc038dea
David Quiñones <daquinons@users.noreply.github.com>
2019-10-07 17:56:17 +0200

    Add stretch count sort algorithm

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -26,6 +26,21 @@ def bubble_sort(arr):
 
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
+    counter = {}
+    for number in range(0, max(arr) + 1):
+        counter[str(number)] = 0
+    for element in arr:
+        counter[str(element)] += 1
+    for index, element in enumerate(counter.keys()):
+        if index > 0:
+            counter[element] += counter[list(counter.keys())[index - 1]]
 
+    counter_copy = counter.copy()
+    for index, element in enumerate(counter.keys()):
+        while counter[element] > counter_copy[list(counter.keys())[index - 1]]:
+            counter[element] -= 1
+            arr[counter[element]] = element
     return arr
 
+array = [1, 3, 4, 7, 9, 2, 1, 8]
+print(count_sort(array))
